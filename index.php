b65be1666a37a6ddab5b307a503eaf2bacdc0458
bellacalista <80676162+bellacalista@users.noreply.github.com>
2021-03-15 16:17:24 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td,
+    th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+  </style>
+</head>
+<?php
+require_once("database.php");
+?>
+<body>
+  <h1>Coba Open Shift ISA Week 5</h1>
+  <h2>Mahasiswa</h2>
+
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>NRP</th>
+    </tr>
+    <?php
+    $sql = "SELECT * FROM mahasiswa";
+    $result = $c->query($sql);
+    if ($result->num_rows > 0) {
+
+      $mhs = array();
+      $i = 0;
+      while ($obj = $result->fetch_assoc()) {
+        echo '<tr>';
+        echo '<td>' . $obj['id'] . '</td>';
+        echo '<td>' . $obj['name'] . '</td>';
+        echo '<td>' . $obj['license'] . '</td>';
+      }
+    } else {
+      echo "Empty table.";
+      die();
+    }
+    ?>
+  </table>
+
+</body>
+
+</html>
